a1bcd2cc74f9c6269f391382085a6aefdf1ab761
rwdthird2019 <59219232+rwdthird2019@users.noreply.github.com>
2020-01-11 14:47:50 +0800

    2020-01-11
    loading修正
    最新消息 navbar 底線去除

--- jquery/loading.js
+++ jquery/loading.js
@@ -1,23 +1,45 @@
 var timer;
 var count = 0;
 var w = $('.load_line').width()
+var h = $('.load_line').offset().top - 100
+var l = -20
+var cut;
+// -20 ~  w - 78
+// w - 78 總共移動距離   (w - 78 +20 ) / 100
+var move
 var oldcount = -200;
 
-// sessionStorage.setItem("hasrun", "false");
+console.log(w);
 
-
+if (w == 423) {
+    cut = 19
+} else if (w == 358) {
+    cut = 21
+} else if (w == 318) {
+    cut = 25
+}
 function load() {
+
+    $('#loadpages').css('opacity', '1')
     var time = Math.floor(Math.random() * 40) + 1
+    move = ((w - 78) / 100) * count
+
+
     $('#load_line_content').css('width', count + "%");
     $('#load_line_txt').text(count + '%')
+
     oldcount = oldcount + (count * 4) - 20
-    $('#bee_img').css({
-        transform: 'translate(' + oldcount + 'px, 0)'
-    });
+
+    $('#bee_img').css('left', count - cut + '%')
+
+
+
     timer = setTimeout(load, time)
     if (count == 100) {
         clearTimeout(timer)
         $('.loadpages').css('opacity', 0)
+        $('#homepage').css('opacity', '1')
+        $('#main').css('opacity', '1')
         setTimeout(() => { $('#loadpages').css('display', 'none') }, 200)
     }
     oldcount = -200;
@@ -31,6 +53,7 @@ if (sessionStorage.getItem("hasrun") == null) {
     sessionStorage.setItem("hasrun", "跑過了");
 
 } else {
-
+    $('#homepage').css('opacity', '1')
+    $('#main').css('opacity', '1')
     setTimeout(() => { $('#loadpages').css('display', 'none') }, 200)
 }
